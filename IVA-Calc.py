7f14cee1c7f9510ebbc0b227fbffd9062343c578
peng81828 <peng81828@users.noreply.github.com>
2015-07-06 21:09:44 -0400

    Initial Commit

---
+++ IVA-Calc.py
@@ -0,0 +1,5 @@
+print ("Welcome to Peng81828's simple tax calculator!")
+input_var = input("Enter the price of your item here: $ ")
+iva= 0.115
+price= (input_var+input_var*iva)
+print ("The price of your item after taxes is $"),price
