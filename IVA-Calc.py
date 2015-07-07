ef14dcfac0c149ea46376dab31326d4b01980a33
peng81828 <peng81828@users.noreply.github.com>
2015-07-06 21:30:29 -0400

    Made it round to the nearest hundredth

--- IVA-Calc.py
+++ IVA-Calc.py
@@ -2,4 +2,5 @@ print ("Welcome to Peng81828's simple tax calculator!")
 input_var = input("Enter the price of your item here: $ ")
 iva= 0.115
 price= (input_var+input_var*iva)
+price= round (price,2)
 print ("The price of your item after taxes is $"),price
