e38022c2de9293b1ac94a51ee0f68034a4ae03a5
peng81828 <peng81828@users.noreply.github.com>
2015-08-23 20:17:58 -0400

    removed while loop

--- IVA-Calc.py
+++ IVA-Calc.py
@@ -1,34 +1,16 @@
 print ("Welcome to Peng81828's Simple Tax & Tip Calculator!")
-goodinput = False
-while not goodinput:
-	try:
-		input_var = float(raw_input("Enter the price of your item here: $ "))
-		goodinput = True
-	except ValueError:
-		print ("Invalid Input! Are you sure that's a number?")
+input_var = float(raw_input("Enter the price of your item here: $ "))
 iva= 0.115
 price= (input_var + input_var * iva)
 price= round (price,2)
-print ("The price of your item after taxes is $"),price
+print ("The price of your item after taxes is $") + price
 tipcalc = raw_input("Would you like to give a tip? ").lower()
 if tipcalc.startswith('n'):
 	print ('Alright!')
-if tipcalc.isdigit():
-	goodinput = False
-	while not goodinput:
-		print ("Only letters please!")
-		tipcalc = raw_input("Would you like to give a tip? ").lower()
-		break
 if tipcalc.startswith('y'):
-	goodinput = False
-	while not goodinput:
-		try:
-			tipcalc = float(raw_input("What percentage would you like to tip? "))
-			goodinput = True
-		except ValueError:
-			print ("Invalid Input! Are you sure that's a number?")
+	tipcalc = float(raw_input("What percentage would you like to tip? "))
 	tipcalc2 = (tipcalc/100)
 	tip= (price + price * tipcalc2)
 	tip= round (tip,2)
-	print ("Your total with a"), tipcalc,("% tip is $"), tip
+	print ("Your total with a") + tipcalc + ("% tip is $") + tip
 print ("Thank you for using Peng81828's Tax & Tip Calculator!")
